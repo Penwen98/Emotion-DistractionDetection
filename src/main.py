bf126c6692d19a786bbf9825020f7a1ef7b3537d
manuandru <manuandru987@gmail.com>
2022-04-09 20:04:37 +0200

    Implement mqtt client

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
+from mqtt_wrapper import MQTTWrapper
 import emotions
 import json
+import sys
+import time
 
-
+client = MQTTWrapper.fromJsonConfig(sys.path[0] + "/mqttConfig.json")
 emotions.setup()
 
-data = {}
-try:
-    data = emotions.getEmotions()
-except emotions.NoFaceDetectedException:
-    print("No face found!")
-
-print(json.dumps(data))
+while True:
+    try:
+       data = emotions.getEmotions()
+       client.publish(json.dumps(data))
+    except emotions.NoFaceDetectedException:
+        print("No face found!")
+    time.sleep(1)
