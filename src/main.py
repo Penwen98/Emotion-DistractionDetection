ac8e6939d56c5b30e2ba3a3c9a462fd9c0b34929
manuandru <manuandru987@gmail.com>
2022-04-28 19:58:23 +0200

    add user config

--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@ from mqtt_wrapper import MQTTWrapper
 import emotions
 import json
 import sys
-import time
+
+def loadUserConfig():
+    with open(sys.path[0] + "/userConfig.json") as json_file:
+        return json.load(json_file)
 
 client = MQTTWrapper.fromJsonConfig(sys.path[0] + "/mqttConfig.json")
+user = loadUserConfig()
 emotions.setup()
 
 while True:
     try:
-       data = emotions.getEmotions()
-       client.publish(json.dumps(data))
+        emotionsMap = emotions.getEmotions()
+        dataToTransmit = { user["user"]: emotionsMap }
+        client.publish(json.dumps(dataToTransmit))
+        print(dataToTransmit)
     except emotions.NoFaceDetectedException:
         print("No face found!")
-    time.sleep(1)
